0314998e5f2706c849206e224ad31760bbe6be4a
Nadhir Boukorsi <gamemn02@gmail.com>
2018-11-17 21:38:50 +0100

    create sign activity

---
+++ app/src/main/java/dz/deepwork/gamemn02/presenceregister/SignActivity.java
@@ -0,0 +1,18 @@
+package dz.deepwork.gamemn02.presenceregister;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+    }
+
+}
