044d22a001c536b66ba6bde62a260830aa083876
Nadhir Boukorsi <gamemn02@gmail.com>
2018-11-17 21:46:21 +0100

    add sign activity robolectric test class

---
+++ app/src/test/java/dz/deepwork/gamemn02/presenceregister/sign/SignActivityTest.java
@@ -0,0 +1,16 @@
+package dz.deepwork.gamemn02.presenceregister.sign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class SignActivityTest {
+
+    @Test
+    public void loadSignInfoBasedOnIdAndPassNumber() {
+
+    }
+}
