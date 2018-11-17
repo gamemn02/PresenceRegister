714065b4663cd3a477d4124aa82ce9753c819e5d
Nadhir Boukorsi <gamemn02@gmail.com>
2018-11-17 22:24:42 +0100

    add member name text in sign activity

--- app/src/main/java/dz/deepwork/gamemn02/presenceregister/sign/SignActivity.java
+++ app/src/main/java/dz/deepwork/gamemn02/presenceregister/sign/SignActivity.java
@@ -6,15 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import dz.deepwork.gamemn02.presenceregister.R;
 
 public class SignActivity extends AppCompatActivity {
 
+    public static final String EXTRA_PASS_NUMBER = "dz.deepwork.gamemn02.extra-pass-number";
+    public static final String EXTRA_ID = "dz.deepwork.gamemn02.extra-id";
+
+    private TextView mMemberNameText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+
+        mMemberNameText = (TextView) findViewById(R.id.tv_member_name);
+        mMemberNameText.setText("teacher1");
     }
 
 }
--- app/src/test/java/dz/deepwork/gamemn02/presenceregister/sign/SignActivityTest.java
+++ app/src/test/java/dz/deepwork/gamemn02/presenceregister/sign/SignActivityTest.java
@@ -1,16 +1,51 @@
 package dz.deepwork.gamemn02.presenceregister.sign;
 
+import android.content.Intent;
+import android.widget.TextView;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.shadows.ShadowApplication;
+
+import dz.deepwork.gamemn02.presenceregister.R;
+import dz.deepwork.gamemn02.presenceregister.login.LoginActivity;
 
 import static org.junit.Assert.*;
+import static org.robolectric.Robolectric.setupActivity;
 
 @RunWith(RobolectricTestRunner.class)
 public class SignActivityTest {
 
-    @Test
-    public void loadSignInfoBasedOnIdAndPassNumber() {
+    // EXAMPLE MEMBER PASS NUMBER AND ID
+    public static final String MEMBER_PASS_NUMBER_EXAMPLE = "123456";
+    private static final long MEMBER_ID_EXAMPLE = 1;
+    // CORRESPONDING MEMBER NAME, REQUIRED ROOM, START TIME, END TIME, DETAILS
+    public static final String MEMBER_NAME_EXAMPLE = "teacher1";
+    private ActivityController<SignActivity> mSignActivityController;
 
+    @Before
+    public void before() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.putExtra(SignActivity.EXTRA_PASS_NUMBER, MEMBER_PASS_NUMBER_EXAMPLE);
+        intent.putExtra(SignActivity.EXTRA_ID, MEMBER_ID_EXAMPLE);
+        mSignActivityController = Robolectric.buildActivity(SignActivity.class, intent).create();
+    }
+
+    @Test
+    public void showMemberNameBasedOnIntent() {
+        SignActivity signActivity = mSignActivityController.get();
+        String actualMemberName =
+                ((TextView) signActivity.findViewById(R.id.tv_member_name)).getText().toString();
+        assertEquals(MEMBER_NAME_EXAMPLE, actualMemberName);
+    }
+
+    @After
+    public void after() {
+        mSignActivityController.destroy();
     }
 }
