0967b5149154f066fd914c07eff3a7f78081974d
Nadhir Boukorsi <gamemn02@gmail.com>
2018-11-19 22:54:19 +0100

    test sign activity loading member info from mocked members repo

--- app/src/test/java/dz/deepwork/gamemn02/presenceregister/sign/SignActivityTest.java
+++ app/src/test/java/dz/deepwork/gamemn02/presenceregister/sign/SignActivityTest.java
@@ -7,29 +7,48 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.android.controller.ActivityController;
-import org.robolectric.shadows.ShadowApplication;
 
+import dz.deepwork.gamemn02.presenceregister.PresenceRegisterApplication;
 import dz.deepwork.gamemn02.presenceregister.R;
-import dz.deepwork.gamemn02.presenceregister.data.MemberRepo;
-import dz.deepwork.gamemn02.presenceregister.login.LoginActivity;
+import dz.deepwork.gamemn02.presenceregister.data.Member;
+import dz.deepwork.gamemn02.presenceregister.data.MembersRepo;
 import dz.deepwork.gamemn02.presenceregister.utils.StringFormats;
 
-import static org.junit.Assert.*;
-import static org.robolectric.Robolectric.setupActivity;
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 public class SignActivityTest {
 
-    // EXAMPLE MEMBER PASS NUMBER AND ID
-    public static final String MEMBER_PASS_NUMBER_EXAMPLE = "123456";
+    // EXAMPLE MEMBER INFO
+    private static final String MEMBER_PASS_NUMBER_EXAMPLE = "123456";
     private static final long MEMBER_ID_EXAMPLE = 1;
+    private static final String MEMBER_FIRST_NAME_EXAMPLE = "first";
+    private static final String MEMBER_LAST_NAME_EXAMPLE = "last";
+    private static final boolean MEMBER_IS_ADMIN_EXAMPLE = false;
+    // CORRESPONDING MEMBER
+    private static final Member sTestedMember = new Member(
+            MEMBER_ID_EXAMPLE,
+            MEMBER_FIRST_NAME_EXAMPLE,
+            MEMBER_LAST_NAME_EXAMPLE,
+            MEMBER_PASS_NUMBER_EXAMPLE,
+            MEMBER_IS_ADMIN_EXAMPLE);
+
     private ActivityController<SignActivity> mSignActivityController;
 
     @Before
     public void before() {
+
+        MembersRepo mockedMembersRepo = mock(MembersRepo.class);
+        when(mockedMembersRepo.findCorrespondingMember(MEMBER_ID_EXAMPLE)).thenReturn(sTestedMember);
+        ((PresenceRegisterApplication) RuntimeEnvironment.application).setMemberRepo(mockedMembersRepo);
+
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.putExtra(SignActivity.EXTRA_PASS_NUMBER, MEMBER_PASS_NUMBER_EXAMPLE);
         intent.putExtra(SignActivity.EXTRA_ID, MEMBER_ID_EXAMPLE);
@@ -40,10 +59,10 @@ public class SignActivityTest {
     public void showMemberNameBasedOnIntent() {
         SignActivity signActivity = mSignActivityController.get();
         TextView memberNameText = (TextView) signActivity.findViewById(R.id.tv_member_name);
+
+        String expectedMemberName = StringFormats.composeName(sTestedMember.getFirstName(), sTestedMember.getLastName());
+
         String actualMemberName = memberNameText.getText().toString();
-        String expectedMemberName = StringFormats.composeName(
-                MemberRepo.MEMBER_FIRST_NAME_EXAMPLE,
-                MemberRepo.MEMBER_LAST_NAME_EXAMPLE);
         assertEquals(expectedMemberName, actualMemberName);
     }
 
