4104c8435f9dc3ff49254ebeb27beeb93b53a702
Nadhir Boukorsi <gamemn02@gmail.com>
2018-11-21 09:32:54 +0100

    create singleton class inside member repo component

--- app/src/main/java/dz/deepwork/gamemn02/presenceregister/data/members/MembersRepoComponent.java
+++ app/src/main/java/dz/deepwork/gamemn02/presenceregister/data/members/MembersRepoComponent.java
@@ -3,10 +3,24 @@ package dz.deepwork.gamemn02.presenceregister.data.members;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import dagger.internal.Preconditions;
 import dz.deepwork.gamemn02.presenceregister.sign.SignActivity;
 
 @Singleton
 @Component(modules = MembersRepoModule.class)
 public interface MembersRepoComponent {
+
     void inject(SignActivity signActivity);
+
+    class Singleton {
+        private static MembersRepoComponent sInstance;
+
+        public static void set(MembersRepoComponent component) {
+            Singleton.sInstance = component;
+        }
+
+        public static MembersRepoComponent getInstance() {
+            return Preconditions.checkNotNull(sInstance);
+        }
+    }
 }
