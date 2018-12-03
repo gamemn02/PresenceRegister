26b328b39aa74eff1e0beb32816514751fcb5ee8
Nadhir Boukorsi <gamemn02@gmail.com>
2018-12-03 22:03:30 +0100

    rename Sign id field to uId

--- app/src/main/java/dz/deepwork/gamemn02/presenceregister/data/signs/Sign.java
+++ app/src/main/java/dz/deepwork/gamemn02/presenceregister/data/signs/Sign.java
@@ -2,15 +2,14 @@ package dz.deepwork.gamemn02.presenceregister.data.signs;
 
 public class Sign {
 
-    public int id;
+    public int uId;
     public String memberName;
     public long signInTime;
     public long signOutTime;
     public String room;
     public String details;
 
-    public Sign(int id, String memberName, long signInTime, long signOutTime, String room, String details) {
-        this.id = id;
+    public Sign(String memberName, long signInTime, long signOutTime, String room, String details) {
         this.memberName = memberName;
         this.signInTime = signInTime;
         this.signOutTime = signOutTime;
