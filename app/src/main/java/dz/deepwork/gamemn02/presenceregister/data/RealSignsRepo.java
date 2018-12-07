3c9bed201c5509a4fb25ca079778bfa20b6f8beb
Nadhir Boukorsi <gamemn02@gmail.com>
2018-12-07 11:51:28 +0100

    add signOut method to real signs repo

--- app/src/main/java/dz/deepwork/gamemn02/presenceregister/data/RealSignsRepo.java
+++ app/src/main/java/dz/deepwork/gamemn02/presenceregister/data/RealSignsRepo.java
@@ -1,14 +1,25 @@
 package dz.deepwork.gamemn02.presenceregister.data;
 
+import dz.deepwork.gamemn02.presenceregister.data.members.MemberDao;
+import dz.deepwork.gamemn02.presenceregister.data.sessions.Session;
+import dz.deepwork.gamemn02.presenceregister.data.sessions.SessionDao;
 import dz.deepwork.gamemn02.presenceregister.data.signins.SignIn;
 import dz.deepwork.gamemn02.presenceregister.data.signins.SignInDao;
+import dz.deepwork.gamemn02.presenceregister.data.signs.Sign;
+import dz.deepwork.gamemn02.presenceregister.data.signs.SignDao;
 
 public class RealSignsRepo {
 
+    private MemberDao mMemberDao;
+    private SessionDao mSessionDao;
     private SignInDao mSignInDao;
+    private SignDao mSignDao;
 
-    public RealSignsRepo(SignInDao signInDao) {
+    public RealSignsRepo(MemberDao memberDao, SessionDao sessionDao, SignInDao signInDao, SignDao signDao) {
+        mMemberDao = memberDao;
+        mSessionDao = sessionDao;
         mSignInDao = signInDao;
+        mSignDao = signDao;
     }
 
     public void signIn(SignIn signIn) {
@@ -17,5 +28,17 @@ public class RealSignsRepo {
 
     public SignIn findSignIn(long sessionId) {
         return mSignInDao.find(sessionId);
+    }
+
+    public void signOut(SignIn signIn) {
+
+        mSignInDao.delete(signIn);
+        Session session = mSessionDao.get(signIn.sessionId);
+        String memberName = mMemberDao.get(session.memberId).name;
+        long signOutTime = signIn.time + 1;
+        String details = session.details;
+        Sign sign = new Sign(memberName, signIn.time, signOutTime, signIn.room, details);
+        mSignDao.insert(sign);
+
     }
 }
--- app/src/test/java/dz/deepwork/gamemn02/presenceregister/data/RealSignsRepoTest.java
+++ app/src/test/java/dz/deepwork/gamemn02/presenceregister/data/RealSignsRepoTest.java
@@ -9,15 +9,31 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import dz.deepwork.gamemn02.presenceregister.data.members.Member;
+import dz.deepwork.gamemn02.presenceregister.data.members.MemberDao;
+import dz.deepwork.gamemn02.presenceregister.data.sessions.Session;
+import dz.deepwork.gamemn02.presenceregister.data.sessions.SessionDao;
 import dz.deepwork.gamemn02.presenceregister.data.signins.SignIn;
 import dz.deepwork.gamemn02.presenceregister.data.signins.SignInDao;
+import dz.deepwork.gamemn02.presenceregister.data.signs.Sign;
 import dz.deepwork.gamemn02.presenceregister.data.signs.SignDao;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RealSignsRepoTest {
 
-    private static final SignIn TEST_SIGN_IN = new SignIn(1, 4, "B007");
-    private static final long TEST_SESSION_ID = 1;
+    private static final Member TEST_MEMBER =
+            new Member("name1", "123456", false);
+    private static final Session TEST_SESSION =
+            new Session(0, 0, TEST_MEMBER.uId, "B007", "Rec E114");
+    private static final SignIn TEST_SIGN_IN =
+            new SignIn(1, 4, "B007");
+    private static final Sign TEST_SIGN =
+            new Sign("name1", 4, 5, "B007", "Rec E114");
+
+    @Mock
+    MemberDao memberDao;
+    @Mock
+    SessionDao sessionDao;
     @Mock
     SignInDao signInDao;
     @Mock
@@ -39,12 +55,27 @@ public class RealSignsRepoTest {
     public void findSignInCallsFind() {
 
         //when
-        Mockito.when(signInDao.find(TEST_SESSION_ID)).thenReturn(TEST_SIGN_IN);
-        SignIn returnedSignIn = realSignsRepo.findSignIn(TEST_SESSION_ID);
+        Mockito.when(signInDao.find(TEST_SIGN_IN.sessionId)).thenReturn(TEST_SIGN_IN);
+        SignIn returnedSignIn = realSignsRepo.findSignIn(TEST_SIGN_IN.sessionId);
 
         //then
-        Mockito.verify(signInDao).find(TEST_SESSION_ID);
+        Mockito.verify(signInDao).find(TEST_SIGN_IN.sessionId);
         Assert.assertEquals(TEST_SIGN_IN, returnedSignIn);
+    }
+
+    @Test
+    public void signOutCallsSignInDaoDeleteAndSignDaoInsert() {
+
+        // TODO use static import when possible
+        //when
+        Mockito.when(memberDao.get(TEST_SESSION.memberId)).thenReturn(TEST_MEMBER);
+        Mockito.when(sessionDao.get(TEST_SIGN_IN.sessionId)).thenReturn(TEST_SESSION);
+        realSignsRepo.signOut(TEST_SIGN_IN);
+
+        //then
+        Mockito.verify(signInDao).delete(TEST_SIGN_IN);
+        Mockito.verify(signDao).insert(TEST_SIGN);
+
     }
 
 
