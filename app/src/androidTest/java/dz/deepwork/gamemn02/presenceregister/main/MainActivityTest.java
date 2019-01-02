419903339555712ee402c7ba99fd080db2fa472e
Nadhir Boukorsi <gamemn02@gmail.com>
2019-01-02 15:08:22 +0100

    show register fragment in main activity on portrait first

--- app/src/androidTest/java/dz/deepwork/gamemn02/presenceregister/main/MainActivityTest.java
+++ app/src/androidTest/java/dz/deepwork/gamemn02/presenceregister/main/MainActivityTest.java
@@ -1,11 +1,19 @@
 package dz.deepwork.gamemn02.presenceregister.main;
 
+import android.content.pm.ActivityInfo;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import dz.deepwork.gamemn02.presenceregister.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -15,12 +23,10 @@ public class MainActivityTest {
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void viewIt() {
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void showRegisterOnPortrait() {
+        mainActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        onView(withId(R.id.layout_register)).check(matches(isDisplayed()));
     }
 
 }
--- app/src/main/java/dz/deepwork/gamemn02/presenceregister/main/MainActivity.java
+++ app/src/main/java/dz/deepwork/gamemn02/presenceregister/main/MainActivity.java
@@ -1,15 +1,33 @@
 package dz.deepwork.gamemn02.presenceregister.main;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
 import dz.deepwork.gamemn02.presenceregister.R;
+import dz.deepwork.gamemn02.presenceregister.login.LoginFragment;
+import dz.deepwork.gamemn02.presenceregister.register.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG_REGISTER_FRAGMENT = RegisterFragment.class.getName();
+    private static final String TAG_LOGIN_FRAGMENT = LoginFragment.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        RegisterFragment registerFragment = (RegisterFragment) fragmentManager.findFragmentByTag(TAG_REGISTER_FRAGMENT);
+        if (registerFragment == null) {
+            registerFragment = new RegisterFragment();
+            fragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_register, registerFragment)
+                    .commit();
+        }
     }
 }
